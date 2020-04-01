02c5236d5804d5952ae1ad5f188266048ede4b4d
Or Avital <oravital7@gmail.com>
2020-04-01 20:02:53 +0300

    FireBase support

--- app/src/main/java/com/example/autopark/MainActivity.java
+++ app/src/main/java/com/example/autopark/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.autopark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.autopark.map.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mapHandlerBtn(View v)
+    {
+        startActivity(new Intent(this, MapsActivity.class));
     }
 }
--- app/src/main/java/com/example/autopark/map/MapsActivity.java
+++ app/src/main/java/com/example/autopark/map/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.example.autopark.map;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.autopark.R;
@@ -16,16 +19,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    private static final int Reques_Code=101;
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int Reques_Code = 101;
     Location mlocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     private GoogleMap mMap;
     float zoomLevel = 16.0f;
+    private final String TAG = "MapTag";
 
 
     @Override
@@ -33,18 +43,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        GetlastLocation();
-
+        getLastLocation();
+        Log.d(TAG, "OnCreate");
+        fillAvailableParking();
     }
 
-    private void GetlastLocation() {
+    private void getLastLocation() {
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location != null){
                     mlocation = location;
-                    Toast.makeText(getApplicationContext() , mlocation.getLatitude()+""+mlocation.getLongitude(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext() , mlocation.getLatitude() + " " + mlocation.getLongitude(),Toast.LENGTH_SHORT).show();
                     SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                             .findFragmentById(R.id.map);
                     supportMapFragment.getMapAsync(MapsActivity.this);
@@ -56,9 +67,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        try {
+            mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.customer_map));
+        } catch (Resources.NotFoundException e)
+        {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
         LatLng latLng = new LatLng(mlocation.getLatitude() , mlocation.getLongitude());
         mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+
+        fillAvailableParking();
+        Log.d(TAG, "onMapReady ");
+
+    }
+
+    private void fillAvailableParking()
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Log.d(TAG, "fillAvailableParking ");
+
+        db.collection("parking").get().addOnCompleteListener( new OnCompleteListener<QuerySnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task)
+            {
+                if (task.isSuccessful())
+                {
+                    for (QueryDocumentSnapshot document : task.getResult())
+                    {
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+                }
+                else
+                {
+                    Log.d(TAG, "Failed: ", task.getException());
+                }
+            }
+        });
     }
 
     @Override
@@ -66,7 +114,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         switch (requestCode){
             case Reques_Code:
                 if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    GetlastLocation();
+                    getLastLocation();
                 }
                 break;
         }
